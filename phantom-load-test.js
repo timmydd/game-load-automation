9a7a807c4dd22224db27878336f348b7c1ef5c53
timmydd <tim.douglas@bbc.co.uk>
2017-07-11 14:47:24 +0100

    Initial Commit

---
+++ phantom-load-test.js
@@ -0,0 +1,16 @@
+var page = require('webpage').create(),
+    system = require('system'),
+    resources = [],
+    address = 'http://play.bbc.co.uk/play/pen/gjkwz4tlrm?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Fthe-worst-witch-magic-adventure-game%3Fembed%3Dtrue&embed=true';
+
+page.open(address, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to load the address!');
+        phantom.exit();
+    } else {
+        setTimeout(function () {
+            page.render(page.title + ".png");
+            phantom.exit();
+        }, 9000); // Change timeout as required to allow sufficient time 
+    }
+});
