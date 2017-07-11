f21f79b18b5e6c6052429ed19a763449f94ea473
timmydd <tim.douglas@bbc.co.uk>
2017-07-11 15:19:51 +0100

    Added an array for URLS

--- phantom-load-test.js
+++ phantom-load-test.js
@@ -1,16 +1,18 @@
 var page = require('webpage').create(),
     system = require('system'),
+    async = require("async"),
     resources = [],
-    address = 'http://play.bbc.co.uk/play/pen/gjkwz4tlrm?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Fthe-worst-witch-magic-adventure-game%3Fembed%3Dtrue&embed=true',
+    myURLS = ['http://play.bbc.co.uk/play/pen/gjkwz4tlrm?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Fthe-worst-witch-magic-adventure-game%3Fembed%3Dtrue&embed=true',
+              'http://play.bbc.co.uk/play/pen/gxfkxn1bkj?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Foperation-ouch-the-snot-apocalypse'],
     timeOut = 6000;
 
-page.open(address, function (status) {
+page.open(myURLS[1], function (status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
         phantom.exit();
     } else {
         setTimeout(function () {
-            page.render(page.title + ".png");
+            page.render('screenshots/' + page.title + '.png');
             phantom.exit();
         }, timeOut);
     }
