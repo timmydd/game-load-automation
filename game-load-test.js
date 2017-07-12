ceac0db5310c979081d78050061ad017ecb94f5e
timmydd <tim.douglas@bbc.co.uk>
2017-07-12 14:17:23 +0100

    Adding GID's for all games...weird errors to do with audio and reference errors...

--- game-load-test.js
+++ game-load-test.js
@@ -1,42 +1,72 @@
 var page = require('webpage').create(),
     system = require('system'),
-    async = require('async'),
-    resources = [],
-    myURLS = ['http://play.bbc.co.uk/play/pen/gxfkxn1bkj?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Foperation-ouch-the-snot-apocalypse',
-              'http://play.bbc.co.uk/play/pen/gjkwz4tlrm?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Fthe-worst-witch-magic-adventure-game%3Fembed%3Dtrue&embed=true'],
+    baseURL = 'http://play.bbc.co.uk/play/pen/',
+    playpenGID = [
+        'g9qw57b51h',
+        'g5t4qgzxny',
+        'gmm269xwnq',
+        'gjkwz4tlrm',
+        'gnkrwjbvsj',
+        'g2zyvrx1k8',
+        'g6ksdxg8cs',
+        'gqgw1yg9x7',
+        'gplxtqsfmf',
+        'g7rh7b3d4q',
+        'g7vwrbdqgl',
+        'g9xktt8lgy',
+        'gwhwzp9c3l',
+        'gbq87ldxnx',
+        'gw5b64r1v4',
+        'g4k4spn8t7',
+        'gldbhs9x5x',
+        'ghdcsb77mr',
+        'g6j9sqqqpv',
+        'g9rjlzj9gm',
+        'gxfkxn1bkj',
+        'gvy3g3mcmz',
+        'gnzr7lndgg',
+        'g1p6k4rr1m',
+        'gdvy86crvl',
+        'g4pjrgd6v2',
+        'gfxdjgry3h',
+        'ggfw5fhksn',
+        'g4g53h7zxq',
+        'gkbqf3gzjf',
+        'gg3rgvg767',
+        'gg65sy3gkw',
+        'gny3l9x6zg',
+        'gbnh54p7hr',
+    ],
     timeOut = 30000;
 
 var timer;
 var i = 0;
 
-    page.onResourceRequested = onResourceRequested;
+page.onResourceRequested = onResourceRequested;
 
-    function onResourceRequested(requestData, networkRequest) {
-        if (isStat(requestData)) {
-            console.log("Stat found", i);
-            console.log(requestData.url);
-            // Paranoia: May keep things a little more stable if stats calls return quickly:
-            networkRequest.changeUrl("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
-        }
-
-        if (isErrorStat(requestData)) {
-            // Browser console.log is returned in stdout anyway, so don't need to scrape page:
-            //console.log(page.plainText);
-            testFinished("fail");
-        }
-        else if (isGameLoadedStat(requestData)) {
-            testFinished("pass");
-        }
+function onResourceRequested(requestData, networkRequest) {
+    if (isStat(requestData)) {
+        // Paranoia: May keep things a little more stable if stats calls return quickly:
+        networkRequest.changeUrl("data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
     }
+
+    if (isErrorStat(requestData)) {
+        // Browser console.log is returned in stdout anyway, so don't need to scrape page:
+        //console.log(page.plainText);
+        testFinished("fail");
+    }
+    else if (isGameLoadedStat(requestData)) {
+        testFinished("pass");
+    }
+}
 
 loadURL();
 
 function loadURL(){
-    page.open(myURLS[i], function (status) {
-        console.log("Starting Test " + myURLS[i]);
+    page.open(baseURL + playpenGID[i], function (status) {
+        console.log("Starting test for GID: " + playpenGID[i]);
         if (status !== 'success') {
-            console.log('Unable to load the address!');
-            testFinished('Bad URL: ' + myURLS[i]);
+            testFinished('Bad URL: ' + playpenGID[i]);
         } else {
 
             timer = setTimeout(function () {
@@ -48,12 +78,12 @@ function loadURL(){
 
 function testFinished(status){
     clearTimeout(timer);
-    console.log('Test finished with: ' + status);
+    console.log('Test: ' + status);
     //page.render('screenshots/' + page.title + '.png');
     // Phantom crashes if exit is not deferred (from onResourceRequested callback, anyway):
     setTimeout(function () {
         i++;
-        if (i >= myURLS.length) {
+        if (i >= playpenGID.length) {
             phantom.exit(0);
 
         } else {
