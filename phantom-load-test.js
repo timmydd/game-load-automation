8bd48e4ad65332544be82743ecd56e9e3c68a3a1
timmydd <tim.douglas@bbc.co.uk>
2017-07-12 09:25:00 +0100

    Initial Commit

--- phantom-load-test.js
+++ phantom-load-test.js
@@ -1,18 +1,21 @@
 var page = require('webpage').create(),
     system = require('system'),
-    async = require("async"),
+    async = require('async'),
     resources = [],
     myURLS = ['http://play.bbc.co.uk/play/pen/gjkwz4tlrm?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Fthe-worst-witch-magic-adventure-game%3Fembed%3Dtrue&embed=true',
               'http://play.bbc.co.uk/play/pen/gxfkxn1bkj?exitGameUrl=http%3A%2F%2Fwww.bbc.co.uk%2Fcbbc%2Fgames%2Foperation-ouch-the-snot-apocalypse'],
-    timeOut = 6000;
+    timeOut = 7000;
 
 page.open(myURLS[1], function (status) {
+    console.log("Starting Tests...");
     if (status !== 'success') {
         console.log('Unable to load the address!');
         phantom.exit();
     } else {
         setTimeout(function () {
+            console.log("Page loaded, taking screenshot...");
             page.render('screenshots/' + page.title + '.png');
+            console.log(page.title + ' Complete');
             phantom.exit();
         }, timeOut);
     }
